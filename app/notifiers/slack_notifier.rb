3e216a2613b614b0aa8fa0d3fe409c7d7a1aaf73
Eric Roos <ericroos13@gmail.com>
2018-12-29 23:18:41 -0600

    add more tests and add vcr

--- app/notifiers/slack_notifier.rb
+++ app/notifiers/slack_notifier.rb
@@ -1,5 +1,18 @@
+require 'faraday'
 class SlackNotifier
-  def notify(msg)
-    return true
+  def notify(msg, webhook_path)
+    webhook_host = 'https://hooks.slack.com'
+    conn = Faraday.new(:url => webhook_host)
+    begin
+      response = conn.post do |req|
+        req.url webhook_path
+        req.headers['Content-Type'] = 'application/json'
+        req.body = {text: msg}.to_json
+      end
+      response.status == 200
+    rescue Faraday::ConnectionFailed => e
+      logger.error e
+      return false
+    end
   end
 end
--- spec/notifiers/slack_notifer_spec.rb
+++ spec/notifiers/slack_notifer_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
+WEBHOOK_PATH = '/services/T9JCZLPPA/BF4MYTNQ7/cJ4Ssv1tMpMzOyqgXtL9HOZz'
+
 describe SlackNotifier do
-  describe '#notify' do
+  describe '#notify', :vcr do
     let(:message) { 'Test Message' }
-    subject { described_class.new.notify(message) } 
-    context 'when we succesffully post to slack' do
+    subject { described_class.new.notify(message, WEBHOOK_PATH) } 
+    context 'with a valid message' do
       it { is_expected.to be true }
+    end
+    context 'with a nil', :vcr do
+      let(:message) { nil }
+      it { is_expected.to be false }
+    end
+    context 'posting to slack throws an exception' do
+      before do
+        allow_any_instance_of(Faraday::Connection).to receive(:post).and_raise(Faraday::ConnectionFailed, 'bad')
+      end 
+      it { is_expected.to be false }
     end
   end
 end
