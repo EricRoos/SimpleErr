ebf5a94069a0dd748373bf6c95e410e2aa61cf35
Eric Roos <ericroos13@gmail.com>
2018-12-30 14:00:05 -0600

    reformat output message to slack

--- app/models/client_app_error.rb
+++ app/models/client_app_error.rb
@@ -1,12 +1,14 @@
 class ClientAppError < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   after_create :notify_parties
 
-  def to_s
-    "#{exception_name} triggered at #{created_at}"
+  def to_s(timezone = 'America/Chicago')
+    "#{exception_name} triggered #{time_ago_in_words(created_at.in_time_zone(timezone))} ago"
   end
 
   def notify_parties
-    webhook_url = '/services/T9JCZLPPA/BF4MYTNQ7/cJ4Ssv1tMpMzOyqgXtL9HOZz'
+    webhook_url = 'https://hooks.slack.com/services/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'
     SlackNotifier.new.tap do |slack|
       slack.notify to_s, webhook_url
     end
--- spec/models/client_app_error_spec.rb
+++ spec/models/client_app_error_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ClientAppError, type: :model do
-  describe '#after_create' do
+  describe 'after_create' do
     describe 'the called methods' do
       subject { described_class.new }
       before do
@@ -9,6 +9,23 @@ RSpec.describe ClientAppError, type: :model do
         subject.save
       end
       it { is_expected }
+    end
+  end
+
+  describe '#to_s' do
+    let(:time_zone){ 'America/New_York' }
+    let(:exception_name){ 'Runtime Error' }
+    let(:client_app_exception) { described_class.new(created_at: created_at, exception_name: exception_name) }
+    subject { client_app_exception.to_s(time_zone) }
+
+    context 'when the error happened just now' do
+      let(:created_at) { Time.current.utc }
+      it { is_expected.to eq "Runtime Error triggered less than a minute ago" }
+    end
+
+    context 'when the error happend 20 minutes ago' do
+      let(:created_at) { (Time.current - 20.minutes).utc }
+      it { is_expected.to eq "Runtime Error triggered 20 minutes ago" }
     end
   end
 end
