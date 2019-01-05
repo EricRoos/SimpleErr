7300a0d241e527658ffff5eb585de992a0ff6fd3
Eric Roos <ericroos13@gmail.com>
2019-01-04 20:37:45 -0600

    debug the base url and request origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :debug_request
+
+  def debug_request
+    Rails.logger.info request.base_url
+    Rails.logger.info request.headers['origin']
+  end
 end
