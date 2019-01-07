c52ff415af24c27f7b88ca2b6b8db9be6656a13c
Eric Roos <ericroos13@gmail.com>
2019-01-06 21:08:55 -0600

    fix spec

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe StaticController, type: :controller do
   describe 'GET #welcome' do
     it 'returns http success' do
       get :welcome
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
